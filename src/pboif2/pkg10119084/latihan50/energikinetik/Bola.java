3338dbec38dffb971bda7dfa219fb608291ac43d
idrismerdefi <67642938+idrismerdefi@users.noreply.github.com>
2020-11-13 10:52:06 +0700

    Update Bola.java

--- src/pboif2/pkg10119084/latihan50/energikinetik/Bola.java
+++ src/pboif2/pkg10119084/latihan50/energikinetik/Bola.java
@@ -4,10 +4,11 @@
  * and open the template in the editor.
  */
 package pboif2.pkg10119084.latihan50.energikinetik;
-
 /**
- *
- * @author MERDEFI
+Nama : Muhammad Idris Merdefi
+Kelas : PBOIF2
+NIM : 10119084
+Deskripsi Program : Program ini berisi program untuk menampilkan perhitungan energi kinetik
  */
 public class Bola {
     private int massa,kecepatan;
